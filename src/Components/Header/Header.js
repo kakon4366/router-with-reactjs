25e525f962d5686dba281af7ad8741211bfc6fcf
kakon4366 <kakonbarman4366@gmail.com>
2022-03-29 15:07:14 +0600

    Navlink added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,10 +7,10 @@ const Header = () => {
 			<Navbar bg="dark" variant="dark">
 				<Container>
 					<Navbar.Brand href="#home">Navbar</Navbar.Brand>
-					<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="#pricing">Pricing</Nav.Link>
+					<Nav className="ms-auto">
+						<Nav.Link href="/">Home</Nav.Link>
+						<Nav.Link href="/about">About</Nav.Link>
+						<Nav.Link href="/friends">Friends</Nav.Link>
 					</Nav>
 				</Container>
 			</Navbar>
