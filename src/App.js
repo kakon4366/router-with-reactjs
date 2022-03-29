5a0e263736db8ead36d7df739315a34aac4abab1
kakon4366 <kakonbarman4366@gmail.com>
2022-03-29 18:20:02 +0600

    Friends Details component added and route link add

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./Components/About/About";
+import FriendDetails from "./Components/FriendDetails/FriendDetails";
 import Friends from "./Components/Friends/Friends";
 import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
@@ -14,6 +15,10 @@ function App() {
 				<Route path="/" element={<Home></Home>}></Route>
 				<Route path="about" element={<About></About>}></Route>
 				<Route path="friends" element={<Friends></Friends>}></Route>
+				<Route
+					path="friend/:friendId"
+					element={<FriendDetails></FriendDetails>}
+				></Route>
 				<Route path="*" element={<NotFound></NotFound>}></Route>
 			</Routes>
 		</div>
---
+++ src/Components/FriendDetails/FriendDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FriendDetails = () => {
+	return (
+		<div>
+			<h2>This component is friend details here</h2>
+			<p>wowwwwwwwwwwwww</p>
+		</div>
+	);
+};
+
+export default FriendDetails;
