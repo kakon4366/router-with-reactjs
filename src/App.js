1d64f40a422621593969df9093a901ab31d9f4db
kakon4366 <kakonbarman4366@gmail.com>
2022-03-29 14:42:56 +0600

    Route page added and Header component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import About from "./Components/About/About";
+import Friends from "./Components/Friends/Friends";
+import Header from "./Components/Header/Header";
 import Home from "./Components/Home/Home";
+import NotFound from "./Components/NotFound/NotFound";
 
 function App() {
 	return (
 		<div className="App">
-			<Home></Home>
+			<Header></Header>
+			<Routes>
+				<Route path="/" element={<Home></Home>}></Route>
+				<Route path="about" element={<About></About>}></Route>
+				<Route path="friends" element={<Friends></Friends>}></Route>
+				<Route path="*" element={<NotFound></NotFound>}></Route>
+			</Routes>
 		</div>
 	);
 }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = () => {
+	return (
+		<div>
+			<h3>Logo</h3>
+			<nav>
+				<ul>
+					<li>Home</li>
+					<li>About</li>
+					<li>Friends</li>
+				</ul>
+			</nav>
+		</div>
+	);
+};
+
+export default Header;
