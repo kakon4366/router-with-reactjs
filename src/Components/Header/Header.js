1c7949310d14c5f03a409a3716936f6c5416c01e
kakon4366 <kakonbarman4366@gmail.com>
2022-03-29 16:26:03 +0600

    without reload page change and link style changed

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,14 +9,26 @@ const Header = () => {
 				<Container>
 					<Navbar.Brand href="/">Logo</Navbar.Brand>
 					<Nav className="ms-auto">
-						<Nav.Link href="/">
-							<Link to="/">Home</Link>
+						<Nav.Link>
+							<Link to="/" className="text-white text-decoration-none">
+								Home
+							</Link>
 						</Nav.Link>
-						<Nav.Link href="/">
-							<Link to="about">About</Link>
+						<Nav.Link>
+							<Link
+								to="about"
+								className="text-white text-decoration-none"
+							>
+								About
+							</Link>
 						</Nav.Link>
-						<Nav.Link href="/about">
-							<Link to="friends">Friends</Link>
+						<Nav.Link>
+							<Link
+								to="friends"
+								className="text-white text-decoration-none"
+							>
+								Friends
+							</Link>
 						</Nav.Link>
 
 						{/* <Nav.Link href="/friends">Friends</Nav.Link> */}
