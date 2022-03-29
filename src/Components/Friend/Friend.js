3ae710db6502b0341f752a8b7cae083d5c37bbb6
kakon4366 <kakonbarman4366@gmail.com>
2022-03-29 16:07:34 +0600

    Navigate dynamic page added

--- src/Components/Friend/Friend.js
+++ src/Components/Friend/Friend.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Friend = ({ friend }) => {
-	const { name, username, email } = friend;
+	const { name, username, email, id } = friend;
+
+	const navigate = useNavigate();
+
+	const showFriendDetails = () => {
+		const path = `/friends/${id}`;
+		navigate(path);
+	};
+
 	return (
 		<div className="border w-25 m-3 p-3">
 			<h3>{name}</h3>
 			<h5>{username}</h5>
 			<p>{email}</p>
-			<button className="btn btn-primary">Show Details</button>
+			<button onClick={showFriendDetails} className="btn btn-primary">
+				Show Details
+			</button>
 		</div>
 	);
 };
