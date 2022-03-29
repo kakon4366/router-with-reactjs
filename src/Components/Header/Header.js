1456fcb884940abaf952e78f68dcad2cac2ce1fc
kakon4366 <kakonbarman4366@gmail.com>
2022-03-29 15:06:03 +0600

    Bootstrap navbar added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,23 +1,20 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 const Header = () => {
 	return (
-		<div>
-			<h3>Logo</h3>
-			<nav>
-				<ul>
-					<li>
-						<a href="/">Home</a>
-					</li>
-					<li>
-						<a href="/about">About</a>
-					</li>
-					<li>
-						<a href="/friends">Friends</a>
-					</li>
-				</ul>
-			</nav>
-		</div>
+		<>
+			<Navbar bg="dark" variant="dark">
+				<Container>
+					<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+					<Nav className="me-auto">
+						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="#features">Features</Nav.Link>
+						<Nav.Link href="#pricing">Pricing</Nav.Link>
+					</Nav>
+				</Container>
+			</Navbar>
+		</>
 	);
 };
 
