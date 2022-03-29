04bef5c4975b352196b76ff9ab900e59d8776a74
kakon4366 <kakonbarman4366@gmail.com>
2022-03-29 15:08:11 +0600

    Logo link added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
 		<>
 			<Navbar bg="dark" variant="dark">
 				<Container>
-					<Navbar.Brand href="#home">Navbar</Navbar.Brand>
+					<Navbar.Brand href="/">Logo</Navbar.Brand>
 					<Nav className="ms-auto">
 						<Nav.Link href="/">Home</Nav.Link>
 						<Nav.Link href="/about">About</Nav.Link>
